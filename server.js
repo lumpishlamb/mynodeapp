835a35a7fd1c6b6996443bb20798f32cb0e3058d
lumpishlamb <phanthelam@gmail.com>
2024-02-18 19:22:53 +0000

    sync test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set up a route for the home page
+// Set up a route for the home page and other pages
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -38,18 +38,3 @@ app.get('/product_2', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-
-
-
-// in lightsail 
-
-// ls  then cd into htdocs
-// g to clear everythin
-// git clone <repo-link>
-// npm instlal 
-
-// in case of error where port is used. 
-// sudo netstat -tulpn | grep :3000
-// node server.js -- to test
-// f -- to go live
