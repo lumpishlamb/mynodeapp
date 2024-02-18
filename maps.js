33bf54514fb13e699f296503fd40a72e01c7ee33
lumpishlamb <phanthelam@gmail.com>
2024-02-18 13:24:05 +0000

    correcting paths

--- maps.js
+++ maps.js
@@ -1,7 +1,7 @@
-import Map from 'ol/Map.js';
-import OSM from 'ol/source/OSM.js';
-import TileLayer from 'ol/layer/Tile.js';
-import View from 'ol/View.js';
+import Map from 'node_modules/ol/Map.js';
+import OSM from 'node_modules/ol/source/OSM.js';
+import TileLayer from 'node_modules/ol/layer/Tile.js';
+import View from 'node_modules/ol/View.js';
 
 const map = new Map({
   layers: [
